f68610e5006c37d72204e39eede758b93a24b683
kamaldhami1010 <kdhami159@gmail.com>
2023-04-18 23:34:56 +0530

    upload api

--- helpers/multer.js
+++ helpers/multer.js
@@ -1,5 +1,15 @@
 const multer = require("multer");
+const fs = require('fs');
+const path = require('path');
 
-const upload = multer({ dest: "public/files" });
+var storage = multer.diskStorage({
+    destination: "./public",
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
 
 module.exports = upload
